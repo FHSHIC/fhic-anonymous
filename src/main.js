e581be3cb84a39088ad8d31e2445aab0f3a907af
qwq-789 <77134549+qwq-789@users.noreply.github.com>
2022-01-30 11:37:17 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+import fontawesome from '@fortawesome/fontawesome'
+import { faTimesCircle, faReply, faLevelUpAlt } from '@fortawesome/fontawesome-free-solid'
+
+fontawesome.library.add(faTimesCircle, faReply, faLevelUpAlt)
